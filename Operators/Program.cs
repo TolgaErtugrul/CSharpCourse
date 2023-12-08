36300475cd41e8429225d07a0c5b500f261289a3
TolgaErtugrul <107127720+TolgaErtugrul@users.noreply.github.com>
2023-12-08 21:13:28 +0300

    Add files via upload

---
+++ Operators/Program.cs
@@ -0,0 +1,40 @@
+﻿//(+ - * / %)
+/*
+int number1= 3;
+int number2= 6;
+
+int total = number1 + number2;
+System.Console.WriteLine("Sayıların toplamı = "+total);
+System.Console.WriteLine("Sayıların farkı = " + (number1-number2));
+*/
+//Kısa ve uzun kenarıbilinen dikdörtgenin alanını hesaplayınız(5 ve 10)
+/*
+int kısaKenar= 5;
+int uzunKenar= 10;
+int alan = kısaKenar * uzunKenar;
+
+System.Console.WriteLine("Dikdörtgenin alanı = "+ alan);
+*/
+//11 sayısını 2 ye bölünüz
+/*
+int bolum = 11 / 2;
+System.Console.WriteLine("11'in 2'ye bölümü = "+ bolum);
+
+//11'in 2 'ye bölümünden kalan kaçtır
+
+int kalan = 11 % 2;
+System.Console.WriteLine("11'in 2'ye bölümünden kalan = "+ kalan);
+*/
+
+int number = 9;
+//number = number+1; number+=1; ikisi aynı işi yapar.
+number++;
+System.Console.WriteLine(number);
+
+System.Console.WriteLine("Sayı ++ = "+ number++);
+System.Console.WriteLine("Yeni Sayı = " +number);
+System.Console.WriteLine("++ Sayı = "+ number);
+
+//++degisken ->artır, yazdır.
+//degisken++ ->yazır, artır.
+
