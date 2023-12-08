b9c6b329bc44ddfe9827de9855dd86259b3e7c57
TolgaErtugrul <107127720+TolgaErtugrul@users.noreply.github.com>
2023-12-08 21:09:36 +0300

    Add files via upload

---
+++ Variables Samples/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+// string mesaj = "Merhaba";
+// System.Console.WriteLine(mesaj);
+
+// string kullanıcıMesaj = Console.ReadLine();
+// System.Console.WriteLine(kullanıcıMesaj);
+
+//Kullanıcıdan adını soyadını ayrı ayrı öğren.
+//alt satırda -> Hoş geldin {ad} {soyad} şeklinde yazdır.
+
+// System.Console.Write("Adınız");
+// string ad= Console.ReadLine();
+// System.Console.Write("Soyadınız");
+// string soyad= Console.ReadLine();
+
+// Console.WriteLine("Hoş geldin "+ ad + " " + soyad);
+
+// //Kullanıcıdan günün mesajını girmesini isteyin
+
+// System.Console.WriteLine("Günün mesajını giriniz");
+// string mesaj = Console.ReadLine();
+// System.Console.WriteLine("Günün mesajı : " + mesaj);
