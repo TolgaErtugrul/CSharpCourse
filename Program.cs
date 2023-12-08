7478910df971ffec82e302f5a751b194d7e34253
TolgaErtugrul <107127720+TolgaErtugrul@users.noreply.github.com>
2023-12-08 21:11:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿//Değişkenler -> Variables
+
+/*
+- Değişkenler basit tabirle, verilerin geçici olarak depolandığı kısımlardır
+
+- Veriler RAM (Random Access Memory) içerisinde depolanır.
+
+Değişken tipleri
+
+-Metinsel
+    *char = ''
+    *string = ""
+
+-Sayısal
+    -Tam Sayılar-
+        *int = 2.1milyar( - , + )
+        *long = 2.1milyar +
+        *short = 32767
+        *byte = 255
+
+    -Ondalık Sayılar
+        *float = 11.4F
+        *double = 11.4
+        *decimal = 11.4M
+
+    -Mantıksal
+        *bool = true ya da false
+
+İsimlendirme : camelCasing
+
+*/
+
+char karakterDegiskeni= 'a';
+char karakterDegiskeni2;
+karakterDegiskeni2 = ',';
+
+string metin = "Merhaba";
+
+byte sayi1 = 33;
+short sayi2 = 1000;
+int sayi3 = 12345;
+long sayi4 = 12345678901;
+
+float piSayisi = 3.14F;
+double kisaKenar = 20.5;
+decimal urunFiyati = 1.4M;
+
+bool silindiMi = false;
+bool yetiskinMi = true;
+
+//Kullanımayacaksa değişken tanımlanmaz.
+
+//Değişken ismi asla iki veya daha fazla kelime aralarında boşluklu olmaz.
+
+//Değişken isimleri rakam ile başlamaz, fakat içerisinde rakam olabilir.
